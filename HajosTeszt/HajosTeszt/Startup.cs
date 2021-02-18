abc5762316a6a8ffdc8addddaa6fe01a3fbaabaa
ritasomlai <rita.somlai@stud.uni-corvinus.hu>
2021-02-18 13:12:24 +0100

    CV

--- HajosTeszt/HajosTeszt/Startup.cs
+++ HajosTeszt/HajosTeszt/Startup.cs
@@ -31,13 +31,13 @@ namespace HajosTeszt
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            /*app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Halihó!");
                 });
-            });
+            });*/
             
             
             
